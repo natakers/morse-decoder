ad80007b287ceb28ba0a8f7ced362f42fc1686a9
natakers <n.kersnovskaya@gmail.com>
2021-08-20 16:31:12 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -38,9 +38,35 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+	var a = expr.match(/.{1,10}/g);
+	var c = [];
+	str = "";
+	for (var i = 0; i <a.length; i++) {
+		var b = a[i].match(/.{1,10}/g);
+		b = b.toString().split("00").join("").match(/.{1,2}/g); 
+		for (var j = 0; j <b.length; j++) {
+			if (b[j] == 10) {
+				b[j] = "."
+			}
+			if (b[j] == 11) {
+				b[j] = "-"
+			} 
+		}
+		 c.push(b.join(""));
+		
+	}
+	 for (var k = 0; k <c.length; k++) {
+		 	c[k] = MORSE_TABLE[c[k]];
+		 	if (!c[k]) {c[k] = " ";}
+		 	str = str + c[k];
+
+		 	
+		 }
+			console.log(str);
+			return str;
 }
 
 module.exports = {
     decode
 }
+
